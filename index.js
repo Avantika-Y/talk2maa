394e383c7ebdae0dc27ea5fe54aa9a58472b8ec1
Avantika-Y <97421516+Avantika-Y@users.noreply.github.com>
2022-11-03 04:16:54 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
+import {getAuth, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDeZjPwMNYJNb8CuX0RhImcbVkaRi25r4Y",
+    authDomain: "talk2maa-5b050.firebaseapp.com",
+    projectId: "talk2maa-5b050",
+    storageBucket: "talk2maa-5b050.appspot.com",
+    messagingSenderId: "177465906093",
+    appId: "1:177465906093:web:ee8a74de5111d8adc98724",
+    measurementId: "G-18R05PEX3P"
+  };
+  
+
+document.getElementById ("btn").addEventListener ("click", signIn, false);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+function signIn(){
+    var email = document.getElementById('eemail').value;
+    var password = document.getElementById('lpassword').value;
+    console.log(email,password)
+    const promise =signInWithEmailAndPassword(auth,email,password);
+    promise.catch(e => {alert(e)});
+    promise.then( e => alert("login successfully"));
+    // window.open("https://www.google.com","_self");
+}
+document.getElementById('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    var userInfo = datab.push();
+    userInfo.set({
+        name: getId('fname'),
+        email : getId('eemail'),
+        password : getId('lpassword')
+    });
+    alert("Successfully Signed Up");
+    console.log("sent");
+    document.getElementById('form').reset();
+});
+function  getId(id){
+    return document.getElementById(id).value;
+}
